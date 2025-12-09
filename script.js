b2ea967308fdb900bb889fdd61fc8ea0d53e9fc2
vincecruzjose13-droid <vincecruzjose13@gmail.com>
2025-12-09 22:59:09 +0800

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function login(){
+  localStorage.setItem("logged","yes");
+  window.location="dashboard.html";
+}
+function register(){
+  alert("Registered!");
+  window.location="login.html";
+}
+if(localStorage.getItem("logged")=="yes"){
+  if(location.pathname.endsWith("login.html")) location="dashboard.html";
+}
 
