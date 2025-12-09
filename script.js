3b51555bde4f866fb3288d306c1e8ebde89436d0
vincecruzjose13-droid <vincecruzjose13@gmail.com>
2025-12-09 23:11:09 +0800

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-function login(){
-  localStorage.setItem("logged","yes");
-  window.location="dashboard.html";
-}
-function register(){
-  alert("Registered!");
-  window.location="login.html";
-}
-if(localStorage.getItem("logged")=="yes"){
-  if(location.pathname.endsWith("login.html")) location="dashboard.html";
-}
+const firebaseConfig={ /* your Firebase config */ };
+firebase.initializeApp(firebaseConfig);
 
+const auth=firebase.auth();
+
+/* LOGIN */
+loginBtn?.addEventListener("click",()=>{
+  auth.signInWithEmailAndPassword(
+    emailInput.value,passwordInput.value
+  ).then(()=>{
+    window.location="index.html";
+  }).catch(e=>{
+    loginError.textContent=e.message;
+  });
+});
+
+/* SIGNUP */
+signupBtn?.addEventListener("click",()=>{
+  auth.createUserWithEmailAndPassword(
+    emailInput.value,passwordInput.value
+  ).then(()=>{
+    window.location="index.html";
+  }).catch(e=>{
+    loginError.textContent=e.message;
+  });
+});
+
+/* Redirect if not logged in */
+auth.onAuthStateChanged(user=>{
+  if(!user && window.location.pathname.includes("index.html")){
+    window.location="login.html";
+  }
+})
